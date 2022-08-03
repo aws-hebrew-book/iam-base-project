dbcc5d202d76fc35a28e2a6ef377c64bd710ece6
EC2 Default User <ec2-user@ip-172-31-79-13.ec2.internal>
2022-08-03 12:16:49 +0000

    wrong lambda call

--- function1/app.py
+++ function1/app.py
@@ -2,5 +2,5 @@ import boto3
 
 def lambda_handler(event, context):
     client = boto3.client('lambda')
-    response = client.invoke(FunctionName="function1-iam")
+    response = client.invoke(FunctionName="function2-iam")
     print(response["Payload"])
